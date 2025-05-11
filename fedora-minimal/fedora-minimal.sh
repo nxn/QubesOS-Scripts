ed040d4ec0b6ca7e8f3223cdf4f570db8faa8bd7
nxn <ernie@nxn.io>
2025-05-11 17:51:34 -0400

    restructures fedora-minimal.sh to allow file review, backups, etc; includes Xresources to make xterm usable on high dpi displays

--- fedora-minimal/fedora-minimal.sh
+++ fedora-minimal/fedora-minimal.sh
@@ -14,56 +14,121 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-set -eu
+set -eu -o pipefail
+
+staging="fedora-minimal"
 
 unpriv(){
   sudo -u nobody "${@}"
 }
 
 download() {
+  directory=$(dirname "${2}")
+  sudo mkdir -p $directory
   unpriv curl -s --proxy http://127.0.0.1:8082 "${1}" | sudo tee "${2}" > /dev/null
 }
 
-# Compliance
-systemctl mask debug-shell.service
-systemctl mask kdump.service
+stage_files() {
+  # Clean previous files
+  sudo rm -rf "./$staging"
 
-# Setting umask to 077
-umask 077
-sudo sed -i 's/^UMASK.*/UMASK 077/g' /etc/login.defs
-sudo sed -i 's/^HOME_MODE/#HOME_MODE/g' /etc/login.defs
-sudo sed -i 's/umask 022/umask 077/g' /etc/bashrc
+  umask 022
+  sudo mkdir -p $staging/etc
+  cat /etc/login.defs | sed 's/^UMASK.*/UMASK 077/g' | sed 's/^HOME_MODE/#HOME_MODE/g' | sudo tee $staging/etc/login.defs > /dev/null
+  cat /etc/bashrc | sed 's/umask 022/umask 077/g' | sudo tee $staging/etc/bashrc > /dev/null
+  
+  # Setup hardened_malloc
+  echo 'libhardened_malloc.so' | sudo tee $staging/etc/ld.so.preload > /dev/null
 
-# Make home directory private
-sudo chmod 700 /home/*
+  # Prepare for SELinux
+  sudo touch $staging/.autorelabel
 
-# Disable timesyncd
-systemctl disable --now systemd-timesyncd
-systemctl mask systemd-timesyncd
+  # Harden SSH
+  download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/ssh/ssh_config.d/10-custom.conf $staging/etc/ssh/ssh_config.d/10-custom.conf
 
-# Harden SSH
-download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/ssh/ssh_config.d/10-custom.conf /etc/ssh/ssh_config.d/10-custom.conf
-chmod 644 /etc/ssh/ssh_config.d/10-custom.conf
+  # Security kernel settings
+  download https://raw.githubusercontent.com/secureblue/secureblue/refs/heads/live/files/system/etc/modprobe.d/blacklist.conf $staging/etc/modprobe.d/workstation-blacklist.conf
+  download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/sysctl.d/99-workstation.conf $staging/etc/sysctl.d/99-workstation.conf
+  
+  # Setup ZRAM
+  download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/systemd/zram-generator.conf $staging/etc/systemd/zram-generator.conf
+  
+  download https://raw.githubusercontent.com/nxn/QubesOS-Scripts/main/etc/X11/Xresources $staging/etc/systemd/zram-generator.conf
+  umask 077
+}
 
-# Security kernel settings
-download https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/modprobe.d/blacklist.conf /etc/modprobe.d/workstation-blacklist.conf
-sudo chmod 644 /etc/modprobe.d/workstation-blacklist.conf
-download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/sysctl.d/99-workstation.conf /etc/sysctl.d/99-workstation.conf
-# Dracut doesn't seem to work - need to investigate
-# dracut -f
-# sudo sysctl -p
+backup_files() {
+  sudo rm -rf "./$staging-backup"
+  sudo mkdir -p "./$staging-backup"
 
-# Setup ZRAM
-download https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/systemd/zram-generator.conf /etc/systemd/zram-generator.conf
+  files=(`find $staging -type f | sed "s/$staging//g"`)
+  for file in "${files[@]}"
+  do
+    if test -f ${file}; then
+      sudo cp --parents ${file} "$staging-backup"
+    fi
+  done
+}
 
-# Install necessary packages
-sudo dnf install -y qubes-core-agent-selinux
+deploy_files() {
+  cd $staging
+  tar cf - * | sudo tar -C / -xf -
+  cd ..
 
-# Setup hardened_malloc
-sudo https_proxy=https://127.0.0.1:8082 dnf copr enable secureblue/hardened_malloc -y
-sudo dnf install -y hardened_malloc
-echo 'libhardened_malloc.so' | sudo tee /etc/ld.so.preload
-sudo chmod 644 /etc/ld.so.preload
+  # Make home directory private
+  chmod 700 /home/*
+}
 
-# Prepare for SELinux
-sudo touch /.autorelabel
+install_packages() {
+  # Install necessary packages
+  sudo dnf install -y qubes-core-agent-selinux
+
+  # Setup hardened_malloc
+  sudo https_proxy=https://127.0.0.1:8082 dnf copr enable secureblue/hardened_malloc -y
+  sudo dnf install -y hardened_malloc
+}
+
+#start_services() { }
+
+stop_services() {
+  # Compliance
+  systemctl mask debug-shell.service
+  systemctl mask kdump.service
+
+  # Disable timesyncd
+  systemctl disable --now systemd-timesyncd
+  systemctl mask systemd-timesyncd
+}
+
+configure() {
+  install_packages
+  stop_services
+  deploy_files
+  #start_services
+
+  # Dracut doesn't seem to work - need to investigate
+  # dracut -f
+  # sudo sysctl -p
+}
+
+case ${1-noop} in
+  configure)
+    configure
+    echo "Configuration complete"
+    ;;
+
+  stage)
+    stage_files
+    echo "Files staged; review before proceeding"
+    ;;
+
+  backup)
+    backup_files
+    echo "Files backed up"
+    ;;
+
+  *)
+    echo 'Specify operation. Run in following order: "stage", "backup", "configure"'
+    ;;
+
+esac
